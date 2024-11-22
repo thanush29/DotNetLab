e3a7e9de90aaf9dace92f85278e112fdcdaeadad
thanush29 <142297909+thanush29@users.noreply.github.com>
2024-11-22 07:53:11 +0530

    Add files via upload

---
+++ STRUCTURE/STRUCTURE/Program.cs
@@ -0,0 +1,39 @@
+﻿using System; 
+namespace @struct 
+{ 
+class Program 
+{ 
+public struct employee 
+{ 
+int id; 
+double salary; 
+public employee(int id, double salary) 
+{ 
+this.id = id; 
+this.salary = salary; 
+} 
+public employee(int id, int salary) 
+{ 
+this.id = id; 
+this.salary = 3400.00; 
+} 
+public employee(employee x) 
+{ 
+this.id = x.id; 
+this.salary = x.salary; 
+} 
+public void DisplayValues() 
+{ 
+Console.WriteLine("Structure"); 
+Console.WriteLine("ID: " + this.id.ToString()); 
+Console.WriteLine("Salary : " + this.salary.ToString()); 
+} 
+} 
+static void Main(string[] args) 
+{ 
+employee emp = new employee(12,4560.00); 
+emp.DisplayValues(); 
+Console.ReadLine(); 
+} 
+} 
+} 
---
+++ Student_Status/Student_Status/Program.cs
@@ -0,0 +1,43 @@
+﻿using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
+namespace studentlist 
+{ 
+struct student 
+{ 
+public string name; 
+public int rollnumber; 
+public string dept; 
+public int mark; 
+public int total, avg; 
+} 
+class Program 
+{ 
+static void Main(string[] args) 
+{ 
+student s; 
+int total = 0; 
+Console.Write("Enter the Name:"); 
+s.name = Console.ReadLine(); 
+Console.Write("Enter the Register Number:"); 
+s.rollnumber = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Enter the Dept.:"); 
+s.dept = Console.ReadLine(); 
+int[] mark = new int[5]; 
+Console.WriteLine("Enter the 5 Subject Marks"); 
+    for (int i = 0; i< 5; i++) 
+{ 
+mark[i] = Convert.ToInt32(Console.ReadLine()); 
+total = total + mark[i]; 
+} 
+Console.WriteLine("Name:" + s.name); 
+Console.WriteLine("Register Number:" + s.rollnumber); 
+Console.WriteLine("Dept.:" + s.dept); 
+Console.WriteLine("Total Marks:" + total); 
+Console.WriteLine("Average:" + total / 5); 
+Console.ReadLine(); 
+} 
+} 
+}
